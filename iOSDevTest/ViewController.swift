262d680b596cc3d317a2dbb43ece15f405b9f752
Oleg Pavlichenkov <=>
2017-11-24 17:06:52 +0300

    initial commit

---
+++ iOSDevTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSDevTest
+//
+//  Created by Oleg Pavlichenkov on 24/11/2017.
+//  Copyright © 2017 Oleg Pavlichenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
